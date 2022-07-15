caa3766945629bdd0b3d53b4b936c545f2b5cd62
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2022-07-14 21:45:22 -0300

    Animation created

--- src/components/gameBoard/GameBoard.jsx
+++ src/components/gameBoard/GameBoard.jsx
@@ -1,11 +1,31 @@
+import { useEffect } from 'react'
+
 import styles from './GameBoard.module.css'
 import Icon from '../icon/Icon'
+import Document from 'next/document'
 
 function GameBoard () {
+  
+  
+  useEffect(() => { 
+  document.addEventListener('keydown', jump) 
+  const Mario = async () => {document.querySelector('.mario')}
+  }, [])
+   
+  const jump = () => {
+    Mario.className.add('jump');
+    
+  } 
+
+    
+  
   return (
-    <div className={styles.gameBoard} >
+    <div className={styles.gameBoard}>
       <div className={styles.pipe}>
         <Icon iconName="pipe" type="png" width="80px" height="75px" />
+      </div>
+      <div className={styles.mario}>
+        <Icon iconName='mario' type='gif' width='120px' />
       </div>
     </div>
   )
