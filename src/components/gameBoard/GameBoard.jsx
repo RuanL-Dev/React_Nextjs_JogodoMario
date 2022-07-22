ffc435de843e8712ab2a5805404095d091f05c48
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2022-07-22 20:41:28 -0300

    classNames and styles created

--- src/components/gameBoard/GameBoard.jsx
+++ src/components/gameBoard/GameBoard.jsx
@@ -1,30 +1,25 @@
-import { useEffect } from 'react'
+import classNames from 'classnames'
+import { useState } from 'react'
 
 import styles from './GameBoard.module.css'
 import Icon from '../icon/Icon'
 import Document from 'next/document'
 
-function GameBoard () {
-  
-  
-  useEffect(() => { 
-  document.addEventListener('keydown', jump) 
-  const Mario = async () => {document.querySelector('.mario')}
-  }, [])
-   
-  const jump = () => {
-    Mario.className.add('jump');
-    
-  } 
+function GameBoard ({ mario }) {
+  const [jump, setJump] = useState(mario)
 
-    
-  
   return (
     <div className={styles.gameBoard}>
+
       <div className={styles.pipe}>
         <Icon iconName="pipe" type="png" width="80px" height="75px" />
       </div>
-      <div className={styles.mario}>
+
+      <div mario tabIndex={0} onKeyDown={() => setJump(!jump) } className={classNames(
+        styles.mario, {
+          [styles.jump]: jump
+        }
+      )}>
         <Icon iconName='mario' type='gif' width='120px' />
       </div>
     </div>
